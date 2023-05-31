4a01e2cb2d864ab837830d74b2fb665d7547118f
Magnus Hasselquist <magnus.hasselquist@gmail.com>
2023-05-31 13:03:54 +0200

    INITIAL

---
+++ mod_hqphpmodule.php
@@ -0,0 +1,9 @@
+<?php
+
+use Joomla\CMS\Helper\ModuleHelper;
+
+defined('_JEXEC') or die;
+
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+
+require ModuleHelper::getLayoutPath('mod_hqphpmodule', $params->get('layout', 'default'));
---
+++ tmpl/default.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('_JEXEC') or die;
+
+// Access to module parameters
+$domain = $params->get('domain', 'https://www.joomla.org');
+?>
+
+<a href="<?php echo $domain; ?>">
+	<?php echo '[PROJECT_NAME]'; ?>
+</a>
